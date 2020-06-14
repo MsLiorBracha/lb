6f483dc776f11c61df9eaa0f3f0c8f9a15995fb9
MsLiorBracha <46165686+MsLiorBracha@users.noreply.github.com>
2020-06-14 22:01:07 +0300

    remove enum from lb

--- loadBalancer.py
+++ loadBalancer.py
@@ -8,14 +8,6 @@ from server import *
 
 servers = range(1, 11)
 
-def from_char_to_service(c):
-    if c == 'M':
-        return Service.MUSIC
-    elif c == 'V':
-        return Service.VIDEO
-    elif c == 'P':
-        return Service.PHOTO
-
 def handle_client(client_sock, client_addr, servers_sockets):
     data = client_sock.recv(2).decode('utf-8')
     if data == '':
@@ -23,8 +15,7 @@ def handle_client(client_sock, client_addr, servers_sockets):
         client_sock.close()
         return
     print >>sys.stderr, 'received "%s" from client %s' % (data, client_addr)
-    service = from_char_to_service(data[1])
-    new_request = Request(int(data[0]), service, data)
+    new_request = Request(int(data[0]), data[1], data)
     new_request.set_client(client_sock, client_addr)
     random_servers = random.sample(servers,  2)
     server1 = servers_sockets[random_servers[0]]
@@ -39,16 +30,16 @@ def handle_client(client_sock, client_addr, servers_sockets):
         server2.add_new_request(new_request)
     
 
-server_addrs = [(('192.168.0.100', 80), Service.VIDEO), \
-     (('192.168.0.101', 80), Service.VIDEO), \
-         (('192.168.0.102', 80), Service.VIDEO), \
-             (('192.168.0.103', 80), Service.VIDEO), \
-        (('192.168.0.104', 80), Service.VIDEO), \
-            (('192.168.0.105', 80), Service.VIDEO), \
-                (('192.168.0.106', 80), Service.MUSIC), \
-            (('192.168.0.107', 80), Service.MUSIC), \
-                (('192.168.0.108', 80), Service.MUSIC), \
-                    (('192.168.0.109', 80), Service.MUSIC)]
+server_addrs = [(('192.168.0.100', 80), 'V'), \
+     (('192.168.0.101', 80), 'V'), \
+         (('192.168.0.102', 80), 'V'), \
+             (('192.168.0.103', 80), 'V'), \
+        (('192.168.0.104', 80), 'V'), \
+            (('192.168.0.105', 80), 'V'), \
+                (('192.168.0.106', 80), 'M'), \
+            (('192.168.0.107', 80), 'M'), \
+                (('192.168.0.108', 80), 'M'), \
+                    (('192.168.0.109', 80), 'M')]
 listening_addr = ('10.0.0.1', 80) # our address for listening to clients
 servers_sockets = {}
 workers = []
